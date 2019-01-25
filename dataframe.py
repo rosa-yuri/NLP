e70967a7179937a45891896bb5a02e6cfee79dc8
rosa-yuri <42934498+rosa-yuri@users.noreply.github.com>
2019-01-26 03:27:48 +0900

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -7,3 +7,9 @@ new_data = pd.DataFrame()
 new_data = opendata[['newsID (S)','createdAt (S)','nation (S)','paragraphs (L)']]
 new_data.columns = ['NewsID','Date','Nation','Context']
 
+data = new_data['Context'].values
+print(str(data[0]))
+
+pd.Series(data).to_json(orient='values')
+data.tostring()
+print(data[0])
